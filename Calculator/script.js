c9f4cc478499b49e39da1c0700bc84540a379228
JasonGuo15 <137559636+JasonGuo15@users.noreply.github.com>
2023-06-25 13:02:50 -0400

    Update script.js
    Added History

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,32 @@
 let ans = ''
+let equation = ''
+const equationList = []
+
+function addHistory() {
+    const div = document.createElement('div');
+
+    div.className = 'historyItem';
+
+    div.innerHTML = `
+        <div>${equation}</div>
+        <div>${ans}</div>
+`;
+
+    const historyContainer = document.getElementById('history');
+    historyContainer.prepend(div);
+}
 
 function evaluateInput(input) {
+    equation = input
     ans = eval(input)
+    equationList.push(String(equation + "=" + ans))
+    addHistory()
 
     try {
         return ans;
     } catch (error) {
         return 'Error';
     }
-
 }
 
 function addOperator(input, operator) {
